48d8bda8637f96bb459e0c71e5d00e7521dacbf0
Davi.Ferigotti <davi.lferigotti@gmail.com>
2018-07-14 19:02:33 -0300

    criado DataHelpers, ListaNegociacoes e mais algumas alterações

--- javascript-avancado-1/client/js/app/controllers/NegociacaoController.js
+++ javascript-avancado-1/client/js/app/controllers/NegociacaoController.js
@@ -5,6 +5,7 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
+    this._listaNegociacoes = new ListaNegociacoes();
   }
 
   adiciona(event){
@@ -13,20 +14,34 @@ class NegociacaoController {
     //a forma abaixo seria a mais fácil de fazer, mas o intrustor preferiu outra para mostrar alumas funções.
     //let data = new Date(this._inputData.value.split('-')); chamamos uma nova data, passando como parametro o valor que foi digitado no formulario. O split serve para divdir a string que o formulário gera em um array separado pelo caracter "-", tornando a string uma data válida
     //na linha acima, poderia também ser usado uma expressão regular para trocar "-" por "," deixando a data no formato que Date() espera receber.
-
-    let data = new Date(... //esses 3 pontinhos são novos do ECMAScript6. É um operador chamado spread operation, que desmembra o array passado para os parâmetros do Date, ou seja, array[0] é o primeiro parâmentro, array[1] é o segundo parâmetro, e assim por diante.
-      this._inputData.value
-      .split('-')
-      .map((item, indice) => item - indice % 2)//.map é um metodo que percorre cada item do array velho, transforma, e cria novo array.
+      //new Date(... //esses 3 pontinhos são novos do ECMAScript6. É um operador chamado spread operation, que desmembra o array passado para os parâmetros do Date, ou seja, array[0] é o primeiro parâmentro, array[1] é o segundo parâmetro, e assim por diante.
+      //this._inputData.value
+      //.split('-') daqui para baixo foi para o arquivo DateHelpers.js
+      //.map((item, indice) => item - indice % 2)//.map é um metodo que percorre cada item do array velho, transforma, e cria novo array.
       //A operação módulo (%) encontra o resto da divisão de um número por outro.
       //foi utilizado novo jeito de declarar função = arrow function (na arrow function, como a função tem só uma instrução, pode tirar o bloco e o "return")
-    );
+    //);
 
-    let negociacao = new Negociacao(
-      data,
-      this._inputQuantidade.value,
-      this._inputValor.value
-    );
-    console.log(negociacao);
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._limpaFormulario();
+
+    console.log(this._listaNegociacoes.negociacoes);
+
+   }
+
+   _criaNegociacao(){
+     return new Negociacao(
+       DateHelper.textoParaData(this._inputData.value),
+       this._inputQuantidade.value,
+       this._inputValor.value
+     );
+   };
+
+   _limpaFormulario(){
+     this._inputData.value = '';
+     this._inputQuantidade.value = 1;
+     this._inputValor.value = 0.0;
+
+     this._inputData.focus();
    }
 }
---
+++ javascript-avancado-1/client/js/app/helpers/DateHelpers.js
@@ -0,0 +1,22 @@
+class DateHelper{
+
+  constructor(){
+    throw new Error('DateHelper não pode ser instanciada')
+  }
+
+  static dataParaTexto(data){
+    return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+  };
+
+  static textoParaData(texto){
+
+    if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+      throw new Error('Deve estar no formato aaaa-mm-dd')
+    }
+
+    return new Date(...texto.split('-').map((item, indice) => item - indice % 2)); //.map é um metodo que percorre cada item do array velho, transforma, e cria novo array.
+    //A operação módulo (%) encontra o resto da divisão de um número por outro.
+    //foi utilizado novo jeito de declarar função = arrow function (na arrow function, como a função tem só uma instrução, pode tirar o bloco e o "return")
+  };
+
+};
