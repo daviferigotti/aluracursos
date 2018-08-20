86168a0121b0cacbae115f630a843e0d9d5b223b
Diego Armando Cacilha <cacilha.tec@gmail.com>
2018-08-19 23:27:15 -0300

    Uma pequena arrumação. Mas muita coisa não entendi.

--- angular/alurapic/config/express.js
+++ angular/alurapic/config/express.js
@@ -1,12 +1,21 @@
-var express = require('express')
-    ,app = express()
-    ,bodyParser = require('body-parser')
-    ,routes = require('../app/routes');
+
+var db = require('./database');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var routes = require('../app/routes')(app);
+var port = 3000;
 
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-routes(app);
+app.listen(port, function(err){
+  if(err){
+    console.log("Erro: " + err);
+  }
+  console.log("Servidor rodando na porta " + port);
+});
 
 module.exports = app;
